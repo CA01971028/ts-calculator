05e60de36077996742690d8657d8ef821eca76b1
CA01971028 <107534406+CA01971028@users.noreply.github.com>
2022-09-21 16:30:28 +0900

    update function
    Adding Factorial and Square Root Functionality

--- src/app/calculator.ts
+++ src/app/calculator.ts
@@ -19,10 +19,20 @@ class Calculator{
     public static pow( opr1: number=8, opr2: number=4):number{
         return opr1 ** opr2
     }
-
+    //平方根(デフォルト指定で８の入力)
+    public static sqrt( opr1: number=4):number{
+        return Math.cbrt(opr1)
+    }
+    //階乗(デフォルト指定で８の入力)
+    public static factorial( opr1: number=8):number{
+        return (opr1 != 1) ? opr1 * this.factorial(opr1 - 1) : 1;
+    }
 }
 console.log(Calculator.add())
 console.log(Calculator.subtract())
 console.log(Calculator.multiply())
 console.log(Calculator.divide())
 console.log(Calculator.pow())
+console.log(Calculator.sqrt())
+console.log(Calculator.factorial())
+
