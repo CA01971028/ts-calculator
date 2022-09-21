1dd36910d1c732256321c99a945caa86939597e7
CA01971028 <107534406+CA01971028@users.noreply.github.com>
2022-09-21 16:16:55 +0900

    update file insert
    Specifying a directory

--- src/app/calculator.ts
+++ src/app/calculator.ts
@@ -15,8 +15,14 @@ class Calculator{
     public static divide( opr1: number=8, opr2: number=4):number{
         return opr1 / opr2
     }
+    //乗算（デフォルト指定で８と４の入力）
+    public static pow( opr1: number=8, opr2: number=4):number{
+        return opr1 ** opr2
+    }
+
 }
 console.log(Calculator.add())
 console.log(Calculator.subtract())
 console.log(Calculator.multiply())
 console.log(Calculator.divide())
+console.log(Calculator.pow())
