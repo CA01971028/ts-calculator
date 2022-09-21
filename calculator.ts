af31aac2cd2653798e1f86d6f52d793ac94856eb
CA01971028 <107534406+CA01971028@users.noreply.github.com>
2022-09-21 15:36:21 +0900

    add function
    Four arithmetic operations created of function.

---
+++ calculator.ts
@@ -0,0 +1,22 @@
+class Calculator{
+    //足し算（デフォルト指定で８と４の入力）
+    public static add( opr1: number=8, opr2: number=4):number{
+        return opr1 + opr2
+    }
+    //引き算(デフォルト指定で８と４の入力)
+    public static subtract( opr1: number=8, opr2: number=4):number{
+        return opr1 - opr2
+    }
+    //掛け算(デフォルト指定で８と４の入力)
+    public static multiply( opr1: number=8, opr2: number=4):number{
+        return opr1 * opr2
+    }
+    //割り算(デフォルト指定で８と４の入力)
+    public static divide( opr1: number=8, opr2: number=4):number{
+        return opr1 / opr2
+    }
+}
+console.log(Calculator.add())
+console.log(Calculator.subtract())
+console.log(Calculator.multiply())
+console.log(Calculator.divide())
